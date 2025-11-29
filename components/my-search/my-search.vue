fb78c597934570ae18a9762865ac4cc3d37068bf
wangqi <wangqi@chicv.com>
2025-11-29 14:39:16 +0800

    first commit

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,49 @@
+<template>
+	<view>
+		<uni-search-bar>
+			
+		</uni-search-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-search",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.my-search-container {
+  display: flex;
+  align-items: center;
+  .my-search-bar {
+    width: 100%;
+  }
+  .my-search-box {
+    height: 36px;
+    background-color: #ffffff;
+    border-radius: 15px;
+    border: 1px solid #c9c9c9;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 $uni-spacing-row-base;
+
+    .icon {
+      width: $uni-img-size-sm;
+      height: $uni-img-size-sm;
+    }
+
+    .placeholder {
+      font-size: $uni-font-size-sm;
+      margin-left: $uni-spacing-row-sm;
+      color: #454545;
+    }
+  }
+}
+</style>
---
+++ pages/hot/hot.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="hot-container">
+	    <!-- logo -->
+		<image class="logo" mode="aspectFit" src="@/static/images/logo.png" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" >
+.hot-container {
+  background-color: $uni-bg-color;
+  .logo {
+    width: 100%;
+    height: 80px;
+  }
+}
+</style>
