2ed061ff2911b686f77d30eabecc4dae71220b16
wangqi <wangqi@chicv.com>
2025-12-01 14:19:05 +0800

    feat: 搜索页面

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
@@ -6,6 +7,18 @@ import App from './App'
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+
+// 导入过滤器
+import * as filters from './filters';
+// 通用样式
+import './styles/global.scss';
+// 文章详情样式
+import './styles/article-detail.scss';
+// 注册过滤器
+Object.keys(filters).forEach((key) => {
+	console.log(key, 'key')
+  Vue.filter(key, filters[key]);
+});
 
 const app = new Vue({
     ...App
