ac21271f0e3caeccc71b4df8e258b42563c2d7ed
wangqi <wangqi@chicv.com>
2025-12-02 14:21:24 +0800

    feat: 文章详情

--- store/index.js
+++ store/index.js
@@ -2,12 +2,14 @@
 import Vuex from 'vuex';
 // 导入 search.js 暴露的对象
 import search from './modules/search';
+import user from './modules/user';
 
 
 // 3. 创建 store 实例
 const store = new Vuex.Store({
   modules: {
-    search
+    search,
+		user,
   }
 });
 export default store;
